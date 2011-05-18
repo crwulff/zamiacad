29a2a9f03e512e95f14e55ec954b659b75dfbd07
Guenter Bartsch <guenter@zamia.org>
2011-05-18 09:19:32 +0200

    add generic dump code to IGItem

---
+++ test/org/zamia/instgraph/IGDumpTest.java
@@ -0,0 +1,114 @@
+/* 
+ * Copyright 2009,2010,2011 by the authors indicated in the @author tag. 
+ * All rights reserved. 
+ * 
+ * See the LICENSE file for details.
+ */
+
+package org.zamia.instgraph;
+
+import org.apache.log4j.Level;
+import org.junit.After;
+import org.junit.Test;
+import org.zamia.BuildPath;
+import org.zamia.SourceFile;
+import org.zamia.Toplevel;
+import org.zamia.ZamiaException;
+import org.zamia.ZamiaLogger;
+import org.zamia.ZamiaProject;
+import org.zamia.ZamiaProjectBuilder;
+import org.zamia.vhdl.ast.DMUID;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Guenter Bartsch
+ */
+public class IGDumpTest {
+
+	public final static ZamiaLogger logger = ZamiaLogger.getInstance();
+
+	private ZamiaProject fZPrj;
+
+	public void setupTest(String aBasePath, String aBuildPath) throws Exception {
+		ZamiaLogger.setup(Level.DEBUG);
+
+		File f = new File(aBuildPath);
+
+		assertTrue(f.exists());
+
+		SourceFile sf = new SourceFile(f);
+
+		fZPrj = new ZamiaProject("IG Test Tmp Project", aBasePath, sf, null);
+		fZPrj.clean();
+	}
+
+	private Toplevel getTL(ZamiaProject aZPrj) {
+		BuildPath bp = aZPrj.getBuildPath();
+		return bp.getToplevel(0);
+	}
+	
+	private DMUID getUID(ZamiaProject aZPrj) {
+		BuildPath bp = aZPrj.getBuildPath();
+
+		Toplevel tl = bp.getToplevel(0);
+
+		return aZPrj.getDUM().getArchDUUID(tl.getDUUID());
+	}
+
+	private void runTest(String aTestDir, String aBuildPathName, int aNumNodes) throws Exception {
+		setupTest(aTestDir, aTestDir + File.separator + aBuildPathName);
+
+		ZamiaProjectBuilder builder = fZPrj.getBuilder();
+
+		builder.build(true, true, null);
+
+		DMUID duuid = getUID(fZPrj);
+
+		int n = fZPrj.getERM().getNumErrors();
+		logger.error("IGDumpTest: Build finished. Found %d errors.", n);
+
+		for (int i = 0; i < n; i++) {
+			ZamiaException em = fZPrj.getERM().getError(i);
+			logger.error("IGDumpTest: error %6d/%6d: %s", i + 1, n, em.toString());
+		}
+
+		assertEquals(0, n);
+
+		n = fZPrj.getIGM().countNodes(duuid);
+		logger.info("IGDumpTest: elaborated model for %s has %d unique modules.", duuid, n);
+		assertEquals(aNumNodes, n);
+		
+		
+		IGModule module = fZPrj.getIGM().findModule(getTL(fZPrj));
+		if (module != null) {
+			
+			module.dump(23);
+			
+		}
+
+		
+	}
+
+	private void runTest(String aTestDir, int aNumNodes) throws Exception {
+		runTest(aTestDir, "BuildPath.txt", aNumNodes);
+	}
+
+	@After
+	public void tearDown() {
+		if (fZPrj != null) {
+			fZPrj.shutdown();
+			fZPrj = null;
+		}
+	}
+
+	@Test
+	public void testLiterals2() throws Exception {
+
+		runTest("examples/semantic/literal2Test", 2);
+	}
+
+}
