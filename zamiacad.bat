ddfbd9f7c35e0ebeb874625789bf78834aad2db1
Valentin <valtih1978@gmail.com>
2013-10-16 15:10:25 +0300

    typos in zamia.sh/bat, inherited classpath, init pythonpath
    Also, shorter classpath since java 6 jar willcards are supported.

--- zamiacad.bat
+++ zamiacad.bat
@@ -1,25 +1,17 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
 
-IF DEFINED ZAMIA_HOME (echo ZAMIA_HOME=%ZAMIA_HOME% is already defined) else (
+:setlocal avoids the classpath inflation when zamiacad.bat is invoked recurrently, many times
+setlocal
+
+IF not DEFINED ZAMIA_HOME (
 	set ZAMIA_HOME=%~dp0
 )
 
+set PYTHONPATH=%ZAMIA_HOME%\share\python\Lib
 
-:echo "ZAMIA_HOME is %ZAMIA_HOME%"
+set CLASSPATH=%CLASSPATH%;%ZAMIA_HOME%bin;%ZAMIA_HOME%share;%ZAMIA_HOME%share\jars\*
 
-set CLASSPATH=%ZAMIA_HOME%bin
-
-
-for %%i in (%ZAMIA_HOME%share\jars\*.jar) do (
-	set CLASSPATH=!CLASSPATH!;%%i
-)
-
-
-set CLASSPATH=%CLASSPATH%;%ZAMIA_HOME%share
-:echo classpath is %CLASSPATH%
-
-
-echo This will start python interpreter withing zamia project. You may run a script by zamia_source("your_script.py") or use %~nx0 --help for other options.
+echo This will start python interpreter within zamia project. You may run a script by zamia_source("your_script.py") or use %~nx0 --help for other options.
 java -Xmx1424m -Xms768m -Xss4m -server org.zamia.cli.Zamia %*
 
+endlocal
--- zamiacad.sh
+++ zamiacad.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-if [ $ZAMIA_HOME = x ] ; then
+#This works with "share" folder unzipped from the zamiacad.jar delivered. 
+#Linux users can mount the zamiacad.jar and avoid unzipping.
+
+#Linux experts: Use of [ instead of [[ is wrong. Comparing $VAR without quotes is wrong. Use of ` is wrong. Using capitals for variable name is wrong.
+#if [ x$ZAMIA_HOME = x ] ; then
+if [[ -z "$ZAMIA_HOME" ]] ; then
   SCRIPT=`readlink -f $0`
   ZAMIA_HOME=`dirname $SCRIPT`
 fi
 
-#echo "ZAMIA_HOME is ${ZAMIA_HOME}"
 
-CP="${ZAMIA_HOME}/bin";
-for i in ${ZAMIA_HOME}/share/jars/*.jar ; do
-  CP="$CP:$i"
-done
-CP="$CP:${ZAMIA_HOME}/share"
+export PYTHONPATH=$ZAMIA_HOME/share/python/Lib
 
-#echo "CLASSPATH is ${CP}"
+#echo CLASSPATH=$CLASSPATH, PYTHONPATH=$PYTHONPATH
 
+export CLASSPATH="$CLASSPATH:${ZAMIA_HOME}/bin:${ZAMIA_HOME}/share:${ZAMIA_HOME}/share/jars/*";
+#echo CLASSPATH=$CLASSPATH
 VMARGS="-Xmx1424m -Xms768m -Xss4m -server"
 
-java $VMARGS -cp $CP org.zamia.cli.Zamia $@
+java $VMARGS org.zamia.cli.Zamia $@
 
